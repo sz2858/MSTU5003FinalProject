98cfa03ec150027029d487fe21a9ac5b1c1e0f0a
sz2858 <65820444+sz2858@users.noreply.github.com>
2020-06-28 16:28:48 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-ityped.init('#ityped', {
-    strings:['We are going to start a journey', 'to explore how computer works', 'and how to work with computer', 'Have Fun ~'],
-    startDelay: 200,
-    loop: true
-  });
