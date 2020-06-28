ef230cb4da0a8c8bf4a8306a041bdc1472af2b5d
sz2858 <65820444+sz2858@users.noreply.github.com>
2020-06-28 17:37:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+ityped.init('#ityped', {
+    strings:['We are going to start a journey', 'to explore how computer works', 'and how to work with computer', 'Have Fun ~'],
+    startDelay: 200,
+    loop: true
+  });
